e0242531c49b68cef7ffcce601f0bcc7a6ae85de
Dalton Harmsen <dylandalton27@gmail.com>
2022-05-08 15:33:05 +0200

    correct policy iteration robot, no early stopping

---
+++ Discrete-Simulations/robot_configs/policy_iteration_robot.py
@@ -0,0 +1,141 @@
+import random
+import numpy as np
+
+max_iter = 200
+
+def robot_epoch(robot):
+    # Initialisation
+    grid = robot.grid
+
+    rewards = {}
+    for i in range(0, grid.n_rows):
+        for j in range(0, grid.n_cols):
+            rewards[(i,j)] = grid.cells[i, j]
+
+    values = rewards.copy()
+
+    actions = {}
+    for i in range(0, grid.n_rows):
+        for j in range(0, grid.n_cols):
+            possible_actions = get_possible_actions(grid, (i, j))
+            # Ensure only keys get added when there are actions
+            if len(possible_actions) != 0:
+                actions[(i, j)] = possible_actions
+            
+
+    # Define an initial policy
+    policy = {}
+
+    for s in actions.keys():
+        try:
+            policy[s] = np.random.choice(actions[s])
+        except: pass
+    # Policy iteration
+    policy = policy_iteration(robot, policy, values, rewards, actions)
+    # Randomly select where to go based on policy
+    best_direction = policy[robot.pos]
+    while robot.orientation != best_direction:
+        robot.rotate('r')
+    robot.move()
+    print('moved', best_direction)
+
+
+def policy_iteration(robot, policy, values, rewards, actions):
+    '''
+    Policy iteration
+
+    Input:
+    robot - a Robot object used to interact with the environment
+    '''
+    print('Policy iteration')
+    for _ in range(max_iter):
+        policy_prev = policy.copy()
+
+        values = policy_evaluation(robot, policy, values, rewards)
+
+        policy = policy_improvement(robot, policy, values, rewards, actions)
+
+        # Early stopping
+        # if all(policy_prev[s] == policy[s] for s in [(i, j) for i in range(robot.grid.n_rows) for j in range(robot.grid.n_cols)]):
+        #     print('stopped early convergence')
+        #     break
+    return policy
+
+
+def policy_evaluation(robot, policy, values, rewards):
+    '''
+    Evaluation of policy
+
+    Input:
+    robot - a Robot object used to interact with the environment
+    '''
+    #print('    Policy evaluation')
+    for _ in range(max_iter):
+        V_prev = values.copy()
+        # Calculate weighted score of state after each possible action
+        for s in policy.keys():
+            a = policy[s]
+            values[s] = rewards[s] + V_prev[get_next_state(s, a)] 
+        # Early stopping
+        # if all(V_prev[s] == values[s] for s in [(i, j) for i in range(robot.grid.n_rows) for j in range(robot.grid.n_cols)]):
+        #     print('stopped early convergence')
+        #     break
+    return values
+
+
+def policy_improvement(robot, policy, values, rewards, actions):
+    '''
+    Improvement update pass in policy
+
+    Input:
+    robot - a Robot object used to interact with the environment
+    '''
+    # print('    Policy improvement')
+    # Calculate Q value
+    Q = {}
+    # Calculate weighted score of each possible action
+    for i in range(robot.grid.n_rows):
+        for j in range(robot.grid.n_cols):
+            s = (i,j)
+            Q = {}
+            try:
+                for a in actions[s]:
+                    Q[a] = rewards[s] + values[get_next_state(s, a)]
+                try:
+                    policy[s] = max(Q, key=Q.get)
+                except: pass
+            except: pass
+
+    return policy
+
+
+def get_next_state(s, a):
+    if a == 'e':
+        return (s[0]+1, s[1])
+    if a == 's':
+        return (s[0], s[1]+1)
+    if a == 'w':
+        return (s[0]-1, s[1])
+    if a == 'n':
+        return (s[0], s[1]-1) 
+
+def get_possible_actions(grid, s):
+    possible_actions = []
+
+    try:
+        if grid.cells[s[0]+1, s[1]] >= 0:
+            possible_actions.append("e")
+    except IndexError: pass
+    try:
+        if grid.cells[s[0], s[1]+1] >= 0:
+            possible_actions.append("s")
+    except IndexError: pass
+    try:
+        if grid.cells[s[0]-1, s[1]] >= 0:
+            possible_actions.append("w")
+    except IndexError: pass
+    try:
+        if grid.cells[s[0], s[1]-1] >= 0:
+            possible_actions.append("n")
+    except IndexError: pass
+    return possible_actions
