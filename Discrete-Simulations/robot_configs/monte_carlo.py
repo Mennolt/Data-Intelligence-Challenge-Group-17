2a5c8e3d52c1396f174a7559bbb217dafbe13b5c
Dalton Harmsen <dylandalton27@gmail.com>
2022-05-13 13:17:58 +0200

    documentation/functional decomposition

--- Discrete-Simulations/robot_configs/monte_carlo.py
+++ Discrete-Simulations/robot_configs/monte_carlo.py
@@ -10,8 +10,35 @@ Questions/comments/whatever:
 """
 
 Q_low, Q_high = -11, 11
+e_soft = True
 
 def robot_epoch(robot):
+    
+    Q, pi, Returns = initialize(robot, Q_low, Q_high, e_soft)
+
+    for epoch in range(epochs):
+        episode = []
+
+
+        if
+
+
+
+def initialize(robot, Q_low : float, Q_high : float, e_soft: bool) -> tuple:
+    '''
+    Initialze Q, pi and returns.
+    
+    For all s in S and a in A:
+        - arbitrary values for Q(s, a) and (e-)soft for pi (a|s)
+        - Returns(s, a): an empty list
+    
+    Input: 
+        robot - Robot object
+        Q_low - float, lowest Q value
+        Q_high - float, highest Q value
+        e_soft - bool, whether to use e-soft policy (True) or soft policy (False)
+
+    '''
     Q = {}
     pi = {}
     Returns = {}
@@ -19,9 +46,12 @@ def robot_epoch(robot):
     epsilon = 0.2
     epochs = 10
 
-    for i in range(0, grid.n_cols):
-        for j in range(0, grid.n_rows):
-            randoms = np.array([random() + epsilon for i in range(4)])
+    for i in range(0, robot.grid.n_cols):
+        for j in range(0, robot.grid.n_rows):
+            if e_soft: 
+                randoms = np.array([random() + epsilon for i in range(4)])
+            else:
+                randoms = np.array([random() for i in range(4)])
             policies = randoms / sum(randoms)
 
             Q[(i,j)] = {'n': randint(Q_low, Q_high), 'e': randint(Q_low, Q_high),
@@ -33,9 +63,4 @@ def robot_epoch(robot):
             Returns[(i, j)] = {'n': 0, 'e': 0,
                                's': 0, 'w': 0}
 
-    for epoch in range(epochs):
-        episode = []
-
-
-        if
-
+    return Q, pi, Returns
