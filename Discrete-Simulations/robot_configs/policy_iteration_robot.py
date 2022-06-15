cedc81f6b38ee59f16805f5c61ab53482e2767c8
Mennolt van Alten <mennoltvanalten01@gmail.com>
2022-06-15 14:37:31 +0200

    Improved rewards for robot in case of charging

--- Discrete-Simulations/robot_configs/policy_iteration_robot.py
+++ Discrete-Simulations/robot_configs/policy_iteration_robot.py
@@ -7,26 +7,34 @@ discount = 0.5
 def robot_epoch(robot):
     # Initialisation
     grid = robot.grid
+    return_battery = 20
 
     rewards = {}
     for i in range(0, grid.n_cols):
         for j in range(0, grid.n_rows):
-
             val = grid.cells[i, j]
-            if val < -2:
-                rewards[(i,j)] = 0
-            elif val == -1 or val == -2:
-                rewards[(i,j)] = -1
-            elif val == 3:
-                rewards[(i, j)] = -10
-            elif val == 2:
-                rewards[(i, j)] = 5
-            elif val == 1:
-                rewards[(i, j)] = 1
-            elif val == 4:
-                rewards[(i, j)] = 100/robot.battery_lvl
+            if robot.battery_lvl > return_battery:
+                if val < -2:
+                    rewards[(i,j)] = 0
+                elif val == -1 or val == -2:
+                    rewards[(i,j)] = -1
+                elif val == 3:
+                    rewards[(i, j)] = -10
+                elif val == 2:
+                    rewards[(i, j)] = 5
+                elif val == 1:
+                    rewards[(i, j)] = 1
+                elif val == 4:
+                    rewards[(i, j)] = 100/robot.battery_lvl
+                else:
+                    rewards[(i, j)] = grid.cells[(i,j)]
             else:
-                rewards[(i, j)] = grid.cells[(i,j)]
+                if val == 4:
+                    rewards[(i,j)] = 1000
+                elif val == -1 or val == -2:
+                    rewards[(i,j)] = -1
+                else:
+                    rewards[(i,j)] = 0
 
     clean_rewards = {}
     for i in range(0, grid.n_cols):
